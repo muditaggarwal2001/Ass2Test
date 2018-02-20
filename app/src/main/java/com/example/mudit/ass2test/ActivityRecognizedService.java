80ca569d57380b44b03cc4c1faf89a1f52d47c7d
muditaggarwal2001 <muditaggarwal2001.muditaggarwal2001@gmail.com>
2018-02-20 02:10:19 -0500

    added image to screen-Error

--- app/src/main/java/com/example/mudit/ass2test/ActivityRecognizedService.java
+++ app/src/main/java/com/example/mudit/ass2test/ActivityRecognizedService.java
@@ -3,7 +3,16 @@ package com.example.mudit.ass2test;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.media.Image;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.widget.ImageView;
+
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.List;
 
 /**
  * Created by Mudit on 07-02-2018.
@@ -24,6 +33,27 @@ public class ActivityRecognizedService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        if (ActivityRecognitionResult.hasResult(intent))
+        {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            System.out.println("insideOnhandlemethod");
+            handleactivity(result.getProbableActivities());
+        }
 
+    }
+
+    private void handleactivity(List<DetectedActivity> probableActivities) {
+        for (DetectedActivity activity : probableActivities)
+        {
+            switch (activity.getType())
+            {
+                case DetectedActivity.WALKING:
+                    break;
+
+                case DetectedActivity.STILL:
+                    MainActivity.ActivityStill();
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mudit/ass2test/MainActivity.java
+++ app/src/main/java/com/example/mudit/ass2test/MainActivity.java
@@ -6,18 +6,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
+    static ImageView imageView;
     public GoogleApiClient googleApiClient;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.activityimage);
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
                 .addConnectionCallbacks(this)
@@ -29,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onConnected(@Nullable Bundle bundle) {
         Intent intent = new Intent(getApplicationContext(),ActivityRecognizedService.class);
         PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(),0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(googleApiClient,3000,pendingIntent);
+        ActivityRecognition.getClient(getApplicationContext()).requestActivityUpdates(3000,pendingIntent);
     }
 
     @Override
@@ -39,6 +44,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        
+        Toast.makeText(getApplicationContext(),"Connection to Google API Failed", Toast.LENGTH_LONG).show();
+    }
+
+    public static void ActivityStill()
+    {
+        imageView.setImageResource(R.drawable.lotus_position);
     }
 }
