f5185b3e63fce51fab5fc9539d977cd7f12ef3a5
muditaggarwal2001 <muditaggarwal2001.muditaggarwal2001@gmail.com>
2018-02-24 13:24:57 -0500

    database error temporarily resolved.

--- app/src/main/java/com/example/mudit/ass2test/MainActivity.java
+++ app/src/main/java/com/example/mudit/ass2test/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        dbHelperClass.close();
+        //dbHelperClass.close();
     }
 
     public class MybroadcastReceiever extends BroadcastReceiver
